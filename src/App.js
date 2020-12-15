16e80b11d1e2bb240e0b6b56f30a89a52ee58c17
Cai Cruz <caicruzz17@gmail.com>
2020-12-14 20:29:21 -0500

    DTB-4 added footer and scroll restoration component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
-import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import {createMuiTheme, ThemeProvider} from '@material-ui/core';
 import PostContainer from './components/PostContainer';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './components/Home';
 
 import './App.css';
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#334772',
-      main: '#fff',
-      // main: '#041c4c',
-      dark: '#001137',
-      contrastText: '#000',
+    palette: {
+        primary: {
+            light: '#334772',
+            main: '#fff',
+            // main: '#041c4c',
+            dark: '#001137',
+            contrastText: '#000',
+        }
     }
-  }
 });
 
 function App() {
-  return (
-    <div className='App'>
-      <ThemeProvider theme={theme}>
-        <Navbar/>
-          <Switch>
-            <Route path='/home'>
-              <Home/>
-            </Route>
-            <Route path='/post/:uri' render={(props) => <PostContainer uri={props.match.params.uri}/>}>
-            </Route>
-          </Switch>
-      </ThemeProvider>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <ThemeProvider theme={theme}>
+                <Navbar/>
+                <Switch>
+                    <Route path='/home'>
+                        <Home/>
+                    </Route>
+                    <Route path='/post/:uri' render={(props) => <PostContainer uri={props.match.params.uri}/>}>
+                    </Route>
+                </Switch>
+                <Footer/>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
