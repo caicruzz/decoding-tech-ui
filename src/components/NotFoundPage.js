ffe0396f1f3bdeec45f70959372d90ed48c33eae
Cai Cruz <caicruzz17@gmail.com>
2020-12-16 19:00:56 -0500

    Made not found page mobile responsive

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-import NotFoundImg from '../404 not found.webp';
+import NotFoundImg from '../logo.png';
 import Image from "material-ui-image";
+import './NotFoundPage.css';
+import {Container} from "@material-ui/core";
 
 export default function NotFoundPage() {
     return (
-        <a href='/home'>
-            <Image src={NotFoundImg} style={{width: '50em', marginTop: '3%', marginLeft: '30%'}} aspectRatio={(3/1)} alt='Not found image'></Image>
-        </a>
+        <Container id='not-found'>
+            <a href='/home'>
+                <Image src={NotFoundImg} aspectRatio={(1/1)} alt='Not found image'></Image>
+            </a>
+            <h1>That page does not exist...</h1>
+            <h3>Click the logo to go home!😊</h3>
+        </Container>
     )
 }
